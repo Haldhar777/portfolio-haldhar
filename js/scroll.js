2c89bf5a218d8d2ff818f950b9c8bdcaf0b5a461
Haldhar Patel <65870132+Haldhar777@users.noreply.github.com>
2022-05-01 00:53:02 +0530

    Add files via upload

---
+++ js/scroll.js
@@ -0,0 +1,21 @@
+const sections = document.querySelectorAll("section");
+const navLink = document.querySelectorAll("header .nav ul li a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (pageYOffset >= sectionTop - sectionHeight / 2) {
+      current = section.getAttribute("id");
+    }
+  });
+  console.log(current);
+  navLink.forEach((a) => {
+    a.classList.remove("active");
+    if (a.classList.contains(current)) {
+      a.classList.add("active");
+    }
+  });
+});
