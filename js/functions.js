67bc8009ebcb0b4e85ca3e9c4c3666da6d48b5ef
Haldhar777 <haldharpatel.hp777@gmail.com>
2022-05-14 16:11:58 +0530

    resolved horizontal overflow
    also resolved toggle btn issue

--- js/functions.js
+++ js/functions.js
@@ -6,15 +6,4 @@ const navlink = document.getElementById("navbar");
 
 const toggle = () => {
   navlink.classList.toggle("close");
-  if (navlink.style.opacity == "1") {
-    navlink.style.opacity = "0";
-  } else {
-    navlink.style.opacity = "1";
-  }
-  navlink.style.right = "0px";
-  navlink.style.top = "40px";
 };
-
-if (window.innerWidth > 950) {
-  navlink.style.opacity = "1";
-}
